78cd885d2ef1e132cfbc6441127a806d8f8c39d2
Teodoro Miranda <Teodoromiranda12@gmail.com>
2023-06-14 21:38:49 -0700

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,40 @@ import { useState } from "react";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  function handleClick() {}
+
   return (
-    <>
-      <div>
-        {puppies.map((puppy) => {
-          return <p>{puppy.name}</p>;
-        })}
-      </div>
-    </>
+    <div className="App">
+      {featPupId}
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
-console.log(puppyList);
+console.log("puppyList: ", puppyList);
+
 export default App;
