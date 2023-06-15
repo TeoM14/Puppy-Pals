37ab4c5010fcd4739de19427fe57496bbf803270
Teodoro Miranda <Teodoromiranda12@gmail.com>
2023-06-14 19:37:39 -0700

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { puppyList } from "./data.js";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+  return (
+    <>
+      <div>
+        {puppies.map((puppy) => {
+          return <p>{puppy.name}</p>;
+        })}
+      </div>
+    </>
+  );
 }
-
+console.log(puppyList);
 export default App;
